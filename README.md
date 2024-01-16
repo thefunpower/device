fbb442c4452975ca31fce277fada4b01101e0ac3
sunkang <13127955213@qq.com>
2024-01-16 14:43:31 +0800

    INIT

--- README.md
+++ README.md
@@ -1,2 +1,54 @@
 # device
+
 获取设备信息
+
+安装
+
+~~~
+composer require thefunpower/device
+~~~
+
+
+使用
+
+~~~
+get_device_info()
+~~~
+
+输出
+~~~ 
+Array
+(
+    [client] => Array
+        (
+            [type] => browser
+            [name] => Chrome
+            [short_name] => CH
+            [version] => 120.0.0.0
+            [engine] => Blink
+            [engine_version] => 120.0.0.0
+            [family] => Chrome
+        )
+
+    [os] => Array
+        (
+            [name] => Windows
+            [short_name] => WIN
+            [version] => 10
+            [platform] => x64
+            [family] => Windows
+        )
+
+    [device] => desktop
+    [brand] => 
+    [model] => 
+    [key] => Windows10 x64 Chrome
+    [uni] => Windows10 x64 Chrome desktop 127.0.0.1
+)
+~~~
+
+## License
+
+[Apache License 2.0](LICENSE)
+
+本项目包含的第三方源码和二进制文件之版权信息另行标注。
---
+++ helper.php
@@ -0,0 +1,25 @@
+<?php 
+
+use DeviceDetector\ClientHints;
+use DeviceDetector\DeviceDetector;
+use DeviceDetector\Parser\Device\AbstractDeviceParser;
+
+function get_device_info(){
+	AbstractDeviceParser::setVersionTruncation(AbstractDeviceParser::VERSION_TRUNCATION_NONE); 
+	$userAgent = $_SERVER['HTTP_USER_AGENT']; 
+	$clientHints = ClientHints::factory($_SERVER); 
+	$dd = new DeviceDetector($userAgent, $clientHints); 
+	$dd->parse(); 
+	if ($dd->isBot()) { 
+	  $botInfo = $dd->getBot();
+	} else {
+	  $data['client'] = $client = $dd->getClient();  
+	  $data['os'] = $os = $dd->getOs();
+	  $data['device'] = $device =  $dd->getDeviceName();
+	  $data['brand'] = $dd->getBrandName();
+	  $data['model'] = $dd->getModel();
+	  $data['key'] = $key = $os['name'].$os['version'].' '.$os['platform'].' '.$client['name'];
+	  $data['uni'] = $key. ' '. $device.' '.get_ip();
+	}
+	return $data;
+}
